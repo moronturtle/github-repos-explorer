d3502edad319335c0a49b25cc1f854039f2f3574
moronturtle <hilmangp@gmail.com>
2025-06-11 17:37:43 +0700

    added daisy ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen">
       <h2 className="text-3xl text-blue-500">welcome to github-repos-explorer</h2>
-    </>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+    </div>
   );
 }
 
